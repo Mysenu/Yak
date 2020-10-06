b3cd184eb91e285e0c0fcb4905c606ae4dd96c6a
Mysenu <Mysenu@users.noreply.github.com>
2020-10-06 23:34:31 +0300

    Fixed "isExpression"

--- src/expression/is_valid_expression.py
+++ src/expression/is_valid_expression.py
@@ -1,7 +1,5 @@
-from typing import Union, Optional
-
-from src.expression.expressions import ScanDirection, SubExpression, SubExpressions
-
+from enum import IntEnum
+from typing import Union, Optional, Tuple
 
 LEFT_UNARY_OPS = set('-+√')
 RIGHT_UNARY_OPS = set('%')
@@ -10,19 +8,91 @@ ALWAYS_BINARY_OPS = BINARY_OPS - (RIGHT_UNARY_OPS | LEFT_UNARY_OPS)
 ROOT_OP = '√'
 PERCENT_OP = '%'
 ALL_OPS = BINARY_OPS | LEFT_UNARY_OPS | RIGHT_UNARY_OPS
-VALID_CHARS = set('0123456789 ') | ALL_OPS
+NUMBERS = set('0123456789 ')
+VALID_CHARS = NUMBERS | ALL_OPS
+
+
+class ScanDirection(IntEnum):
+    Left = 1
+    Right = 2
+
+
+class SubExpression:
+    """The class is implemented as an object for ease of working with subexpression and their range."""
+    def __init__(self, range: tuple = (0, 0), expr: str = '') -> None:
+        self.__range = range
+        self.__expr = expr
+
+    @property
+    def range(self) -> Tuple[int, int]:
+        return self.__range
+
+    @property
+    def expr(self) -> str:
+        return self.__expr
+
+    @property
+    def start(self) -> int:
+        return self.__range[0]
+
+    @property
+    def end(self) -> int:
+        return self.__range[1]
+
+    @property
+    def subexpr(self) -> str:
+        start, end = self.__range
+        return self.__expr[start:end]
+
+    def __add__(self, other):
+        return SubExpressions(self.__range, other.__range, self.subexpr, other.subexpr)
+
+    def __str__(self):
+        return self.subexpr
+
+
+class SubExpressions:
+    """The class is implemented as an object for ease of working with subexpressions and their ranges."""
+    def __init__(self, range_1: tuple, range_2: tuple, subexpr_1: str, subexpr_2: str) -> None:
+        self.__range = min(range_1[0], range_2[0]), max(range_1[1], range_2[1])
+        self.__subexprs = subexpr_1, subexpr_2
+
+    @property
+    def range(self) -> Tuple[int, int]:
+        return self.__range
+
+    @property
+    def subexprs(self) -> Tuple[str, str]:
+        return self.__subexprs
+
+    @property
+    def start(self) -> int:
+        return self.__range[0]
+
+    @property
+    def end(self) -> int:
+        return self.__range[1]
+
+    def __str__(self) -> str:
+        return ', '.join(self.__subexprs)
+
+    def __len__(self) -> int:
+        return 2
+
+    def __getitem__(self, index: int) -> str:
+        return self.__subexprs[index]
 
 
 def findOperand(expr: str,
                 index: int,
-                direction: int = ScanDirection.Right) -> Union[SubExpression, None]:
+                direction: int = ScanDirection.Right) -> Union[SubExpression, bool]:
     """
     Finds the range of a subexpression at the specified index and
     direction, and returns an object of the Subexpression class.
     """
     print(expr, ' | findOperand', ' | Index: ', index)
     if not expr:
-        return
+        return None
 
     if direction == ScanDirection.Right:
         scan_left = False
@@ -36,6 +106,9 @@ def findOperand(expr: str,
         index -= 1
         open_bracket = ')'
         close_bracket = '('
+
+    if index < 0:
+        return None
 
     start_pos = index
     if expr[index] == open_bracket:
@@ -104,6 +177,10 @@ def findOperand(expr: str,
 def findOperands(expr: str, index: int) -> SubExpressions:
     left_part = findOperand(expr, index, ScanDirection.Left)
     right_part = findOperand(expr, index, ScanDirection.Right)
+
+    if None in (left_part, right_part):
+        return None
+
     return left_part + right_part
 
 
@@ -131,6 +208,9 @@ def isBinaryOperation(text: str, index: int) -> bool:
 def isValidOperand(operand: Union[str, SubExpression]) -> Optional[bool]:
     if isinstance(operand, SubExpression):
         operand = str(operand)
+
+    if operand is None:
+        return None
 
     try:
         float(operand)
@@ -171,11 +251,14 @@ def isExpression(text: str) -> bool:
     if set(text) - VALID_CHARS:
         return False
 
-    operation_count = 0
+    if not set(text) - NUMBERS:
+        return False
+
     operand_count = 0
     bracket_count = 0
     char_is_digit = False
     char_is_procent = False
+    first_operand = False
 
     try:
         index = 0
@@ -198,35 +281,43 @@ def isExpression(text: str) -> bool:
                 char_is_procent = False
 
             if isOperation(text, index):
-                operation_count += 1
-                if isValidOperation(text, index) is False:
+                is_valid_operation = isValidOperation(text, index)
+                if is_valid_operation is False or is_valid_operation is None:
                     print('isExpression: ', text[index])
                     return False
                 if isBinaryOperation(text, index):
+                    first_operand = False
                     operand_count = 0
-            elif char == ' ':
-                pass
             elif char == '(':
                 bracket_count += 1
             elif char == ')':
                 bracket_count -= 1
-            else:
+            elif char == ' ' and text[max(index - 1, 0)].isdigit():
+                first_operand = True
+                operand_count += 1
+                if operand_count > 1:
+                    print('False operand count 1')
+                    return False
+            elif char == ' ':
+                pass
+
+            if char.isdigit() and first_operand:
                 operand_count += 1
                 if operand_count > 1:
                     return False
 
             if bracket_count < 0:
+                print('False bracket count')
                 return False
             index += 1
     except SyntaxError:
-        return False
-
-    if operation_count < 1:
+        print('False syntaxError')
         return False
 
     if bracket_count != 0:
+        print('False bracket count')
         return False
 
     return True
 # print(isExpression('√48-1+3'))
-print(isExpression('1+(3+4)-2'))
+# print(isExpression('√9'))
