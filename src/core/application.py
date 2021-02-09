7eeb516156c5291e9093464171266d8dac7b709e
Mysenu <Mysenu@users.noreply.github.com>
2021-02-09 22:47:37 +0300

    Added a style sheet for message 'Restore history'

--- src/core/application.py
+++ src/core/application.py
@@ -13,11 +13,11 @@ class Application(QApplication):
 
         self.main_window = MainWindow()
 
-        if self.needToRestoreHistory():
-            self.addHistoryCacheToHistory()
-
         with open(getResourcePath("main.qss"), 'r', encoding='utf-8') as file_with_style_sheet:
             self.setStyleSheet(file_with_style_sheet.read())
+
+        if self.needToRestoreHistory():
+            self.addHistoryCacheToHistory()
 
         self.main_window.show()
 
@@ -25,7 +25,14 @@ class Application(QApplication):
         if not CACHE_FILE.exists() or (CACHE_FILE.stat().st_size <= 0):
             return False
 
-        answer = QMessageBox.question(self.main_window, 'Restore history', 'Restore calculation history?')
+        message = QMessageBox(self.main_window)
+        message.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        message.setWindowTitle('Restore history')
+        message.setText('Restore calculation history?')
+        message.setIcon(QMessageBox.Question)
+
+        answer = message.exec()
+
         return answer == QMessageBox.Yes
 
     def addHistoryCacheToHistory(self) -> None:
